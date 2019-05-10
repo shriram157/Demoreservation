8c6f809e6bf4db69529330f7143105b409d44261
Anubah Pandey <anubha_pandey@toyota.ca>
2019-05-10 05:24:47 +0000

    ldap change

--- demoreservation/webapp/controller/Home.controller.js
+++ demoreservation/webapp/controller/Home.controller.js
@@ -12,6 +12,7 @@ sap.ui.define([
 			this.getRouter().attachRoutePatternMatched(this.onRouteMatched, this);
 			this.initialFilter();
 			this.initSecurity();
+			this.fetchLDAPuserdata();
 		},
 		initialFilter: function () {
 			this.getView().byId("zoneFilter").setSelectedKey("3000");
@@ -281,6 +282,34 @@ sap.ui.define([
 		
 		
 		
+		},
+		
+		fetchLDAPuserdata:function(){
+			// this.UserData = new sap.ui.model.json.JSONModel();
+			// this.getView().setModel(this.UserData, "UserDataModel");
+			// sap.ui.getCore().setModel(this.UserData, "UserDataModel");
+			var that = this;
+
+			//	sap.ui.core.BusyIndicator.show();
+				$.ajax({
+				dataType: "json",
+				url: "/demoreservation-node/node/tci/internal/api/v1.0/security/ldap/rest/getUserByUID",
+				type: "POST",
+				success: function (userAttributes) {
+		//			sap.ui.core.BusyIndicator.hide();
+					// console.log("User Attributes", userAttributes);
+					// that.UserData.setProperty("/FirstName",userAttributes.samlAttributes.FirstName);
+					// that.UserData.setProperty("/LastName",userAttributes.samlAttributes.LastName);
+					// that.UserData.setProperty("/Email",userAttributes.samlAttributes.Email);
+					// that.UserData.setProperty("/Userid",userAttributes.userProfile.id);
+					// sap.ui.core.BusyIndicator.hide();
+					// that.UserData.updateBindings(true);
+					// that.UserData.refresh(true);
+				},
+				error: function (oError) {
+		//			sap.ui.core.BusyIndicator.hide();
+				}
+			});
 		}
 
 	});
