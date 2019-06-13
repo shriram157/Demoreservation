06a4a89f8d0843f53adaffe8f42dd950f6c01b59
Aarti Dhamat <aarti_dhamat@toyota.ca>
2019-06-13 15:43:06 +0000

    commit- change in logic for edit reservation button as per oData flag

--- demoreservation/webapp/controller/VehicleDetails.controller.js
+++ demoreservation/webapp/controller/VehicleDetails.controller.js
@@ -2,7 +2,7 @@ sap.ui.define(["ca/toyota/demoreservation/demoreservation/controller/BaseControl
 	"use strict";
 	return BaseController.extend("ca.toyota.demoreservation.demoreservation.controller.VehicleDetails", {
 		onInit: function () {
-		//	this.getRouter().attachRoutePatternMatched(this.onRouteMatched, this);
+			//	this.getRouter().attachRoutePatternMatched(this.onRouteMatched, this);
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.getRoute("VehicleDetails").attachMatched(this.onRouteMatched, this);
 
@@ -10,10 +10,14 @@ sap.ui.define(["ca/toyota/demoreservation/demoreservation/controller/BaseControl
 		onRouteMatched: function (oEvent) {
 			var oArgs;
 			oArgs = oEvent.getParameter("arguments");
+			var oLocalModel = new sap.ui.model.json.JSONModel({
+				"enableEditBtn": false
+			});
+			this.getView().setModel(oLocalModel, "LocalModel");
 			this.getVehicleData(oArgs.vguid);
 		},
 		onNavButtonPress: function (oEvent) {
-		//	this.doRoute("Home");
+			//	this.doRoute("Home");
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("Home");
 		},
@@ -21,25 +25,28 @@ sap.ui.define(["ca/toyota/demoreservation/demoreservation/controller/BaseControl
 			//	this.doRoute("RequestDetail");
 			var vhvin = this.getView().getModel().getData().VehicleDetailSet.VHVIN;
 			var Zresreq = this.getView().getModel().getData().VehicleDetailSet.ZRESREQ;
-			if(Zresreq===""){
+			if (Zresreq === "") {
 				// msg
-			}else{
-			//	this.doReqRoute("RequestDetail",vhvin,Zresreq);
+			} else {
+				//	this.doReqRoute("RequestDetail",vhvin,Zresreq);
 				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-				oRouter.navTo("RequestDetail",{vhvin:vhvin, Zresreq:Zresreq});
+				oRouter.navTo("RequestDetail", {
+					vhvin: vhvin,
+					Zresreq: Zresreq
+				});
 			}
 
 		},
 		getVehicleData: function (VHVIN) {
 			var email = sap.ui.getCore().getModel("UserDataModel").getData().Email;
 			//testing
-	//		email = "anubha_pandey@toyota.ca";
+			//		email = "anubha_pandey@toyota.ca";
 			var uri = "/demoreservation-node/node/Z_VEHICLE_DEMO_RESERVATION_SRV_02/";
 			var that = this,
-			sPath = "/VehicleDetailSet(VHVIN='" + VHVIN + "',Email='" + email + "')?$expand=NAVFACOPTION,NAVDEALEROPTION",
-		//	var OrderChangeModel = that.getOwnerComponent().getModel("DemoOdataModel");
+				sPath = "/VehicleDetailSet(VHVIN='" + VHVIN + "',Email='" + email + "')?$expand=NAVFACOPTION,NAVDEALEROPTION",
+				//	var OrderChangeModel = that.getOwnerComponent().getModel("DemoOdataModel");
 				oDetailModel = new sap.ui.model.odata.ODataModel(uri, true);
-		//	oDetailModel = 	that.getOwnerComponent().getModel("DemoOdataModel");
+			//	oDetailModel = 	that.getOwnerComponent().getModel("DemoOdataModel");
 			var oBusyDialog = new sap.m.BusyDialog();
 			oBusyDialog.open();
 			// read OData model data into local JSON model 
@@ -47,39 +54,44 @@ sap.ui.define(["ca/toyota/demoreservation/demoreservation/controller/BaseControl
 				method: "GET",
 				success: function (oData, oResponse) {
 					var oJSONModel = new sap.ui.model.json.JSONModel();
-					
+					if (oData.No_edit === "") {
+						this.getView().getModel("LocalModel").setProperty("enableEditBtn", true);
+					} else {
+						this.getView().getModel("LocalModel").setProperty("enableEditBtn", false);
+					}
+
 					// extract Requestor type text
 					var mod = that.getOwnerComponent().getModel("vehicles"),
 						i,
 						objR = mod.getContext("/FilterData/RequestorType").getObject();
-						for(i=0 ;i< objR.length;i++){ 
-							if(objR[i].key === oData.ZZREQTYP) { 
-								oData.ZZREQTYPTXT =  objR[i].name;
-							}     
+					for (i = 0; i < objR.length; i++) {
+						if (objR[i].key === oData.ZZREQTYP) {
+							oData.ZZREQTYPTXT = objR[i].name;
 						}
+					}
 
 					// extract Purchaser type text
-					var	objP = mod.getContext("/FilterData/PurchaserType").getObject();
-						for(i=0 ;i< objP.length;i++){ 
-							if(objP[i].key === oData.ZZPURTYP) { 
-								oData.ZZPURTYPTXT =  objP[i].name;
-							}     
+					var objP = mod.getContext("/FilterData/PurchaserType").getObject();
+					for (i = 0; i < objP.length; i++) {
+						if (objP[i].key === oData.ZZPURTYP) {
+							oData.ZZPURTYPTXT = objP[i].name;
 						}
+					}
 
 					oJSONModel.setData({
 						VehicleDetailSet: oData
 					});
-					if(oData.ZRESREQ ===""){
+					if (oData.ZRESREQ === "") {
 						// Reservation request not exists for this vehicle, so enable "Reserve" button
 						that.byId("btnReserve").setVisible(true);
-						that.byId("btnEdit").setVisible(false);
+						// that.byId("btnEdit").setVisible(false);
 						// no reservation exists
 						that.byId("pageReservation").setVisible(false);
 						that.byId("pageReservation").setTitle("No reservation exists");
-					}else{
+					} else {
 						// Reservation request exists for this vehicle, so enable "Edit" button
 						that.byId("btnReserve").setVisible(false);
-						that.byId("btnEdit").setVisible(true);
+						// that.byId("btnEdit").setVisible(true);
 						that.byId("pageReservation").setVisible(true);
 						that.byId("pageReservation").setTitle("Reservation Details");
 					}
@@ -101,9 +113,12 @@ sap.ui.define(["ca/toyota/demoreservation/demoreservation/controller/BaseControl
 		 */
 		_onCreatePress: function (oEvent) {
 			var vhvin = this.getView().getModel().getData().VehicleDetailSet.VHVIN;
-		//	this.doReqRoute("RequestDetail",vhvin,"C");
+			//	this.doReqRoute("RequestDetail",vhvin,"C");
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.navTo("RequestDetail",{vhvin:vhvin, Zresreq:"C"});
+			oRouter.navTo("RequestDetail", {
+				vhvin: vhvin,
+				Zresreq: "C"
+			});
 
 		}
 	});
